a3d5fbd4660bc59504af21fc89275116ff698790
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-14 05:01:40 +0530

    Added: Static method to print result

--- src/main/java/com/bridegelabz/mathoperation/MathOperationApp.java
+++ src/main/java/com/bridegelabz/mathoperation/MathOperationApp.java
@@ -3,6 +3,10 @@ package com.bridegelabz.mathoperation;
 @FunctionalInterface
 interface IMathFunction {
 	int calculate(int a, int b);
+	static void printResult(int a,int b,String function,IMathFunction functionObject) {
+		
+		System.out.println("Result of "+function+" is "+functionObject.calculate(a, b));
+	}
 }
 
 public class MathOperationApp {
@@ -17,5 +21,9 @@ public class MathOperationApp {
 		System.out.println("multiplication " + multiply.calculate(2, 3));
 		System.out.println("subtraction " + subtract.calculate(2, 3));
 		System.out.println("division " + divide.calculate(4, 2));
+		System.out.println();
+		IMathFunction.printResult(2, 3, "Subtraction ", subtract);
+		IMathFunction.printResult(2, 3, "Multiplication ", multiply);
+		IMathFunction.printResult(4, 2, "Division ", divide);
 	}
 }
